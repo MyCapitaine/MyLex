a775f7fafd9d50148fb37e09d6771fa2364c5040
MyCapitaine <1051943988@qqq.com>
2015-10-24 19:01:41 +0800

    RE转NFA完成

--- NFA.rb
+++ NFA.rb
@@ -2,36 +2,79 @@
 #
 #it only works when use right posfixExpression, obviously)
 class NFA
-    #each nodes will be sign up and save in this list 
+    #each nodes will be sign up and save in this list
     attr_reader :nodeList
     #num of begin and end node
     attr_reader :begin, :end
 	def initialize(postfix)
-		print postfix, "\n"
+		#print postfix, "\n"
+        
 		@nodeList = []
-        
         nodeSetStack = []
-        
         posfix.each_char { |ch|
             case ch
-            when '+', '*'
+            when '+'
+                #not support now
+            when '*'
+                oldNodeSet = nodeSetStack.pop
                 
-            when '|', '#'
+                newTail = Node.new([], nil, nil)
+                @nodeList << newTail
+                newTailNum = @nodeList.size
+                newHead = Node.new([oldNodeSet.headNum, newTailNum], nil. nil)
+                @nodeList << newHead
+                newHeadNum = @nodeList.size
+                
+                @nodeList[oldNodeSet.tailNum].upsilonRefList << oldNodeSet.headNum << newTailNum
+                
+                nodeSetStack << NodeSet.new(newHeadNum, newTailNum)
+                
+            when '|'
+                nodeSet1 = nodeSetStack.pop
+                nodeSet2 = nodeSetStack.pop
+                
+                newHead = Node.new([nodeSet1.headNum, nodeSet2.headNum], nil, nil)
+                @nodeList << newHead
+                newHeadNum = @nodeList.size
+                newTail = Node.new([], nil, nil)
+                @nodeList << newTail
+                newTailNum = @nodeList.size
+                
+                @nodeList[nodeSet1.tailNum].upsilonRefList << newTailNum
+                @nodeList[nodeSet2.tailNum].upsilonRefList << newTailNum
+                newNodeSet = NodeSet.new(newHeadNum, newTailNum)
+                nodeSetStack << newNodeSet
+                
+            when '#'
+                nodeSetPost = nodeSetStack.pop
+                nodeSetPre = nodeSetStack.pop
+                @nodeList[nodeSetPre.tailNum].upsilonRefList << nodeSetPost.headNum
+                
+                newNodeSet = NodeSet.new(nodeSetPre.headNum, nodeSetPost.tailNum)
+                nodeSetStack << newNodeSet
                 
             else
+                tail = Node.new([], nil, nil)
+                @nodeList << tail
+                tailNum = @nodeList.size
+                head = Node.new([], ch, tailNum)
+                @nodeList << head
+                headNum = @nodeList.size
+                nodeSetStack << NodeSet.new(headNum, tailNum)
                 
             end
         }
         
-
+        @begin = nodeSetStack[0].headNum
+        @end = nodeSetStack[0].tailNum
+        
 	end
-
-
-
 
 
 	def toDFA
 	end
+    
+    
     
 end
 
@@ -46,8 +89,8 @@ class Node
     #save the key ref and value/number of ref 
     #have no refKey&refValue when refKey = nil
     attr_reader :refKey, :refValue
-    def initialize(upsilonRefList = [], refKey = nil, refValue = nil)
-        @upsilonRefList = upsilonRefList
+    def initialize(upsilonRefList, refKey, refValue)
+        @upsilonRefList = upsilonRefList == nil? [] : upsilonRefList
         @refKey = refKey
         @refValue = refValue
     end
@@ -60,10 +103,10 @@ struct of nodeSet
 save num of head and tail node
 =end
 class NodeSet
-    attr_reader :head, :tail
-    def initialize(head, tail)
-        @head = head
-        @tail = tail
+    attr_reader :headNum, :tailNum
+    def initialize(headNum, tailNum)
+        @headNum = headNum
+        @tailNum = tailNum
     end
 end
 
